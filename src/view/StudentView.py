e0af72d5c82946d9c6074c57cc2eeb2c8d1ce467
NicolasLeidi <71295543+NicolasLeidi@users.noreply.github.com>
2024-07-09 20:53:45 -0300

    Eliminado colores de frames

--- src/view/StudentView.py
+++ src/view/StudentView.py
@@ -18,13 +18,13 @@ class StudentView(View):
         self.__create_widgets(root)
     
     def __create_widgets(self, root):
-        self._upper_side_frame = HeaderFrame(root, bg="blue", width=800, height=50, pady=3, padx=10, functions=(self._load_knowledge_base, self._test_mode, self._manual_create_mode))
-        self._middle_side_knowledge_base_frame = KnowledgeBaseFrame(root, bg="red", width=320, pady=3, padx=5)
-        self._middle_side_testing_frame = TestingFrame(root, bg="pink", width=480, pady=3, padx=5, functions=(self._handle_test_text_box_click))
-        self._middle_side_manual_creating_frame = ManualCreatingFrame(root, bg="yellow", width=480, pady=3, padx=5)
-        self._middle_side_loaded_examples_frame = LoadedExamplesFrame(root, bg="purple", width=480, pady=3, padx=5)
-        self._lower_side_testing_frame = FooterTestingFrame(root, bg="green", width=800, height=50, pady=3, functions=(self._test_solution, self._clean_tests, self._pop_test))
-        self._lower_side_batch_creating_frame = FooterCreatingFrame(root, bg="cyan", width=800, height=50, pady=3, variables=(self._ordered, self._first_only), functions=(self._add_example, self._save_examples, self._clean_examples, self._pop_examples))
+        self._upper_side_frame = HeaderFrame(root, width=800, height=50, pady=3, padx=10, functions=(self._load_knowledge_base, self._test_mode, self._manual_create_mode))
+        self._middle_side_knowledge_base_frame = KnowledgeBaseFrame(root, width=320, pady=3, padx=5)
+        self._middle_side_testing_frame = TestingFrame(root, width=480, pady=3, padx=5, functions=(self._handle_test_text_box_click))
+        self._middle_side_manual_creating_frame = ManualCreatingFrame(root, width=480, pady=3, padx=5)
+        self._middle_side_loaded_examples_frame = LoadedExamplesFrame(root, width=480, pady=3, padx=5)
+        self._lower_side_testing_frame = FooterTestingFrame(root, width=800, height=50, pady=3, functions=(self._test_solution, self._clean_tests, self._pop_test))
+        self._lower_side_batch_creating_frame = FooterCreatingFrame(root, width=800, height=50, pady=3, variables=(self._ordered, self._first_only), functions=(self._add_example, self._save_examples, self._clean_examples, self._pop_examples))
         
         # Place the frames in their respective places
         
